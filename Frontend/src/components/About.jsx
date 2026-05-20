0e9899cc0e589569f53648e6baa444a383d8774e
dolisha <dolishagandhi@gmail.com>
2026-05-20 23:06:10 +0530

    new commit

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from './Hero'
 import {motion} from 'framer-motion'
-import backdrop from '../assets/backdrop.jpg'
+import backdrop from '../assets/dolisha.jpeg'
 
 const About = () => {
   return (
