799f92d5d5fcea8dc9fe7f592726e5d9a942642b
dolisha <dolishagandhi@gmail.com>
2026-05-21 10:57:30 +0530

    new commit

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -12,11 +12,33 @@ const Contact = () => {
     className='py-20 bg-dark-200'
     >
         <div className='container mx-auto px-6'>
-            <h2>
+            <h2 className='text-3xl font-bond text-center mb-4'>
                 Get In
-                <span>Touch</span>
+                <span className='text-purple-600'>Touch</span>
             </h2>
-            <p>Have Projects in mind or want to collaborate ? Let's talk!</p>
+            <p className='text-gray-400 text-center max-w-2xl mx-auto mb-16'>Have Projects in mind or want to collaborate ? Let's talk!</p>
+            
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-12 max-w-5xl mx-auto'>
+               <div>
+                   <form className='space-y-6'>
+                    <div>
+                       <label htmlFor='name' className='block text-gray-300 mb-2'>Your Name</label>
+                       <input className='w-full bg-dark-300 border border-dark-400 rounded-lg px-4 py-3 outline-none' type='text' />
+                    </div>
+                    <div>
+                       <label htmlFor='email' className='block text-gray-300 mb-2'>Email Address</label>
+                       <input className='w-full bg-dark-300 border border-dark-400 rounded-lg px-4 py-3 outline-none' type='text' />
+                    </div>
+                    <div>
+                       <label htmlFor='message' className='block text-gray-300 mb-2'>Your Message</label>
+                       <input className='w-full bg-dark-300 border border-dark-400 rounded-lg px-4 py-20 outline-none' type='text' />
+                    </div>
+                    <button type='submit' className='w-full px-6 py-3 bg-purple-600 rounded-lg font-medium hover:bg-purple-600 transition duration-300 cursor-pointer' >Submit</button>
+                   </form>
+               </div>
+            </div>
+
+
         </div>
         
       
